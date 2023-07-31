415fb4dab8001264aada6ca59ffd82a3084b9b2d
Ion Minascurta <ion.minascurta18@gmail.com>
2023-08-01 00:41:36 +0300

    I finished the functionality for New Appointment option fromn Dentist Menu
     - Dentist now can add and remove certain interventions in NewAppointment form;
     - Data is dynamically modyfiing in both interfaces and database;
     - Payment form is fully functional;
     - Made some validations that avoid overpayment;
     - At the end a receipt is issued.

--- DentalClinic/Bill.cs
+++ DentalClinic/Bill.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,38 @@ namespace DentalClinic
 {
     public partial class Bill : Form
     {
-        public Bill()
+        private string connStr = "server=localhost;user=root;database=sincretic_bd;port=3306;password=root;pooling=true";
+        private ulong id_receipt;
+
+        public Bill(ulong id)
         {
             InitializeComponent();
+            this.id_receipt = id;
+        }
+
+        private void Bill_Load(object sender, EventArgs e)
+        {
+            lblBillid.Text = id_receipt.ToString();
+            using(MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                string query = "CALL generate_receipt(@id_receipt)";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id_receipt", id_receipt);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                rdr.Read();
+                lblAppointmentID.Text = rdr.GetUInt64(0).ToString();
+                lblDate.Text = rdr.GetMySqlDateTime(1).Day + "/" + rdr.GetMySqlDateTime(1).Month + "/" + rdr.GetMySqlDateTime(1).Year;
+                lblPatientID.Text = rdr.GetUInt64(2).ToString();
+                lblSurnameName.Text = rdr.GetString(3);
+                lblPaidAmount.Text = rdr.GetFloat(4) + " Lei";
+                rdr.Close();
+            }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
--- DentalClinic/NewAppointment.cs
+++ DentalClinic/NewAppointment.cs
@@ -1,4 +1,5 @@
 ﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Asn1.X500;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,7 +15,10 @@ namespace DentalClinic
     public partial class NewAppointment : Form
     {
         private string connStr = "server=localhost;user=root;database=sincretic_bd;port=3306;password=root;pooling=true";
-        ulong id_appointment;
+        private ulong id_appointment;
+        private float materials_cost;
+        private float charge;
+
         public NewAppointment(ulong id)
         {
             InitializeComponent();
@@ -38,6 +42,34 @@ namespace DentalClinic
                     rdr.NextResult();
                 }
                 rdr.Close();
+
+                string query1 = "SELECT * FROM consultatie_interventie " +
+                    "INNER JOIN interventii USING(id_interventie)" +
+                    "WHERE id_consultatie = \"" + id_appointment + "\"";
+                cmd = new MySqlCommand(query1, conn);
+                rdr = cmd.ExecuteReader();
+                while (rdr.HasRows)
+                {
+                    while (rdr.Read())
+                    {
+                        lbInterventions.Items.Add(rdr.GetString(3));
+                        materials_cost += rdr.GetFloat(4);
+                        charge += rdr.GetFloat(5);
+                    }
+                    rdr.NextResult();
+                }
+                rdr.Close();
+
+                string query2 = "SELECT * FROM consultatii WHERE id_consultatie = " + id_appointment;
+                cmd = new MySqlCommand(query2, conn);
+                rdr = cmd.ExecuteReader();
+                rdr.Read();
+                txtComments.Text = rdr.GetString(6);
+                rdr.Close();
+
+                lbInterventions.Refresh();
+                txtMaterialCost.Text = materials_cost.ToString() + " Lei";
+                txtCharge.Text = charge.ToString() + " Lei";
             }
         }
 
@@ -51,24 +83,74 @@ namespace DentalClinic
 
         private void btnPayment_Click(object sender, EventArgs e)
         {
-            Form f = new Payment();
-            f.ShowDialog();
+            float total_fee = 0f;
+            float paid_fee = 0f;
+            using(MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                string query = "SELECT * FROM consultatii WHERE id_consultatie = " + id_appointment;
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                rdr.Read();
+                total_fee = rdr.GetFloat(4);
+                paid_fee = rdr.GetFloat(5);
+                rdr.Close();
+            }
+            if(total_fee == paid_fee)
+            {
+                MessageBox.Show("This appointment has been paid.", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                if (lbInterventions.Items.Count == 0)
+                {
+                    MessageBox.Show("There is no intervention selected!", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    using (MySqlConnection conn = new MySqlConnection(connStr))
+                    {
+                        string comments = txtComments.Text;
+                        conn.Open();
+                        string query = "CALL update_consultatie(@id_appointment, @total_charge, @comments)";
+                        MySqlCommand cmd = new MySqlCommand(query, conn);
+                        cmd.Parameters.AddWithValue("@id_appointment", id_appointment);
+                        cmd.Parameters.AddWithValue("@total_charge", materials_cost + charge);
+                        cmd.Parameters.AddWithValue("@comments", comments);
+                        cmd.ExecuteNonQuery();
+                    }
+                    Form f = new Payment(id_appointment);
+                    f.ShowDialog();
+                }
+            }
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            string searched_type = cmbIntervention.SelectedValue.ToString();
+            cmbIntervention.Refresh();
+            string searched_type = cmbIntervention.Items[cmbIntervention.SelectedIndex].ToString();
             int cnt_interventions = (int)nmrIntervention.Value;
             using (MySqlConnection conn = new MySqlConnection(connStr))
             {
+                conn.Open();
                 string query1 = "CALL cautare_interventie(@type)";
                 MySqlCommand cmd = new MySqlCommand(query1, conn);
-                conn.Open();
+                cmd.Parameters.AddWithValue("@type", searched_type);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                rdr.Read();
                 ulong id_to_add;
-                _ = ulong.TryParse(cmd.ExecuteScalar().ToString(), out id_to_add);
+                _ = ulong.TryParse(rdr.GetUInt64(0).ToString(), out id_to_add);
+                materials_cost += (cnt_interventions * rdr.GetFloat(2));
+                charge += (cnt_interventions * rdr.GetFloat(3));
+                txtMaterialCost.Text = materials_cost.ToString() + " Lei";
+                txtCharge.Text = charge.ToString() + " Lei";
+                txtMaterialCost.Refresh();
+                txtCharge.Refresh();
+                rdr.Close();
                 for (int i = 0; i < cnt_interventions; i++)
-                {
-                    cmbIntervention.Items.Add(searched_type);
+                { 
+                    lbInterventions.Items.Add(searched_type);
+                    lbInterventions.Refresh();
                     string query2 = "CALL insert_consultatie_interventie(@id_appointment, @id_intervention)";
                     cmd = new MySqlCommand(query2, conn);
                     cmd.Parameters.AddWithValue("@id_appointment", id_appointment);
@@ -76,6 +158,45 @@ namespace DentalClinic
                     cmd.ExecuteNonQuery();
                 }
             }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            string deleted_type = lbInterventions.SelectedItem.ToString();
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                string query = "CALL cautare_interventie(@type)";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@type", deleted_type);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                rdr.Read();
+                ulong id_to_delete;
+                _ = ulong.TryParse(rdr.GetUInt64(0).ToString(), out id_to_delete);
+                materials_cost -= rdr.GetFloat(2);
+                charge -= rdr.GetFloat(3);
+                txtMaterialCost.Text = materials_cost.ToString() + " Lei";
+                txtCharge.Text = charge.ToString() + " Lei";
+                txtMaterialCost.Refresh();
+                txtCharge.Refresh();
+                rdr.Close();
+
+                string query1 = "DELETE FROM consultatie_interventie " +
+                    "WHERE id_interventie = " + id_to_delete +
+                    " AND id_consultatie = " + id_appointment + " LIMIT 1";
+                cmd = new MySqlCommand(query1, conn);
+                cmd.ExecuteNonQuery();
+                lbInterventions.Items.RemoveAt(lbInterventions.SelectedIndex);
+                lbInterventions.Refresh();
+            }
+        }
+
+        private void NewAppointment_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            txtMaterialCost.Text = "";
+            txtCharge.Text = "";
+            materials_cost = 0f;
+            charge = 0f;
         }
     }
 }
